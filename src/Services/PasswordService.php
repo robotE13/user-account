da6418ad7ed7ee92b1b6180669dc4bf42b56e102
Evgenii Dudal <wolfstrace@gmail.com>
2020-04-17 22:22:40 +0300

    remove acceptance add gitkeep to functional

---
+++ src/Services/PasswordService.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the user-account.
+ *
+ * Copyright 2020 Evgenii Dudal <wolfstrace@gmail.com>.
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ * @package user-account
+ */
+
+namespace RobotE13\UserAccount\Services;
+
+/**
+ * Description of PasswordService
+ *
+ * @author Evgenii Dudal <wolfstrace@gmail.com>
+ */
+class PasswordService
+{
+    //put your code here
+}
