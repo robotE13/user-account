b2a327d5c1ad373276ff7e9fd2fbf0d0e3563ec8
Evgenii Dudal <wolfstrace@gmail.com>
2020-06-20 15:28:52 +0300

    service for user

---
+++ src/Repositories/UserRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the user-account.
+ *
+ * Copyright 2020 Evgenii Dudal <wolfstrace@gmail.com>.
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ * @package user-account
+ */
+
+namespace RobotR13\UserAccount\Repositories;
+
+use RobotE13\UserAccount\Entities\User;
+
+/**
+ *
+ * @author Evgenii Dudal <wolfstrace@gmail.com>
+ */
+interface UserRepository
+{
+    public function findById($id):User;
+    public function findByEmail($email):User;
+
+    public function add(User $user);
+    public function update(User $user);
+    public function remove($id);
+}
---
+++ src/Services/UserService.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * This file is part of the user-account.
+ *
+ * Copyright 2020 Evgenii Dudal <wolfstrace@gmail.com>.
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ * @package user-account
+ */
+
+namespace RobotE13\UserAccount\Services;
+
+use RobotR13\UserAccount\Repositories\UserRepository;
+use RobotE13\UserAccount\Entities\{
+    Id,
+    Password,
+    Status,
+    User,
+    UserStatuses
+};
+
+/**
+ * Description of UserService
+ *
+ * @author Evgenii Dudal <wolfstrace@gmail.com>
+ */
+class UserService
+{
+
+    private $users;
+
+    public function __construct(UserRepository $users)
+    {
+        $this->users = $users;
+    }
+
+    public function create(UserDTO $dto): User
+    {
+        $user = new User(
+                new Id(),
+                $dto->email,
+                new Password($dto->password),
+                new Status(
+                        UserStatuses::UNCONFIRMED,
+                        new UserStatuses()
+                )
+        );
+        $this->users->add($user);
+        return $user;
+    }
+
+    public function register($dto)
+    {
+
+    }
+
+    public function activate($id)
+    {
+
+    }
+
+    public function suspend($id)
+    {
+
+    }
+
+}
